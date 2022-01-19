c508faa947aeaa89512808c66402992c8ac4b6f0
Facundo Saldivia <facundosaldivia@gmail.com>
2022-01-19 19:07:57 -0300

    hash added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './shared/guards/auth.guard';
+import { SignedGuard } from './shared/guards/signed.guard';
 
 const routes: Routes = [
   {
     path: 'auth',
-
+    canActivate: [SignedGuard],
     loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
   },
   {
@@ -18,7 +19,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
